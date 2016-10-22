2de2d1e062045877aafffe964e17d0c59ae67f11
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-22 14:47:55 -0700

    fixes email button

--- public/app.js
+++ public/app.js
@@ -3,6 +3,17 @@ console.log("app.js linked");
 
 $(document).ready(function() {
 
+  // navbar dropdown
+  // $('.dropdown-toggle').dropdown('.dropdown-menu')
+
+  // $('.dropdown-toggle').dropdown('.project-list', function () {
+  //   console.log("you clicked the dropdown");
+  // })
+
+
+
+
+  // contact me section toggle
   $('#contact-me-button').on('click', function(event) {
     event.preventDefault();
     //shows contact me div
@@ -10,10 +21,10 @@ $(document).ready(function() {
     //sets current position on the page and scrolls down
     var y = $(window).scrollTop();
     $(window).scrollTop(y+200);
+
+    $('.email-button').on('click', function(event) {
+      window.location.href = "mailto:mishaleclair@gmail.com";
+    });
   });
 
-});
-
-$('.email-button').on('click', function(event) {
-  window.location.href = "mailto:mishaleclair@gmail.com";
 });
