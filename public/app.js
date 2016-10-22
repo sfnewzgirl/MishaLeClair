b30d0e5b01b799cb729ade060e839ad54ec341c7
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-22 14:23:44 -0700

    initial boostrap grid

--- public/app.js
+++ public/app.js
@@ -1,20 +1,19 @@
 // sanity check
-// console.log("app.js linked");
-//
-// $(document).ready(function() {
-//
-//   console.log('you clicked contact me');
-//   event.preventDefault();
-//   $('#contact-me-button').on('click', function(event) {
-//     //shows contact me div
-//     $('.contact-options').show();
-//     //sets current position on the page and scrolls down
-//     var y = $(window).scrollTop();
-//     $(window).scrollTop(y+200);
-//   });
-//
-// });
-//
-// $('.email-button').on('click', function(event) {
-//   window.location.href = "mailto:mishaleclair@gmail.com";
-// });
+console.log("app.js linked");
+
+$(document).ready(function() {
+
+  $('#contact-me-button').on('click', function(event) {
+    event.preventDefault();
+    //shows contact me div
+    $('.contact-options').toggle();
+    //sets current position on the page and scrolls down
+    var y = $(window).scrollTop();
+    $(window).scrollTop(y+200);
+  });
+
+});
+
+$('.email-button').on('click', function(event) {
+  window.location.href = "mailto:mishaleclair@gmail.com";
+});
