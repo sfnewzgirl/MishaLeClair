6b55e2893b645379a7bc466bb7ae4ad2402b912e
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-24 09:34:14 -0700

    hide show working for project sections

--- public/app.js
+++ public/app.js
@@ -5,11 +5,20 @@ $(document).ready(function() {
 
   $('.about-me-nav').on('click', function(event) {
     console.log("you clicked about me");
+    $('#about-me').show();
+    $("#sitter-swap-container").hide();
+    $('#within-reach-container').hide();
+    $('#protips-container').hide();
+    $('#race-time-container').hide();
   });
 
   $('.contact-me-nav').on('click', function(event) {
     console.log("you clicked contact me");
-      $('#about-me').toggle();
+      $('#about-me').show();
+      $("#sitter-swap-container").hide();
+      $('#within-reach-container').hide();
+      $('#protips-container').hide();
+      $('#race-time-container').hide();
   });
 
   // $('.project-dropdown-nav').on('click', function(event) {
@@ -18,33 +27,37 @@ $(document).ready(function() {
 
   $('.sitter-swap-dropdown').on('click', function(event) {
     console.log("you clicked sitterswap");
+    $('#about-me').hide();
     $("#sitter-swap-container").show();
     $('#within-reach-container').hide();
-    $('protips-container').hide();
+    $('#protips-container').hide();
     $('#race-time-container').hide();
   });
 
   $('.within-reach-dropdown').on('click', function(event) {
     console.log("you clicked within reach");
+    $('#about-me').hide();
     $("#sitter-swap-container").hide();
     $('#within-reach-container').show();
-    $('protips-container').hide();
+    $('#protips-container').hide();
     $('#race-time-container').hide();
   });
 
   $('.protips-dropdown').on('click', function(event) {
     console.log("you clicked protips");
+    $('#about-me').hide();
     $("#sitter-swap-container").hide();
     $('#within-reach-container').hide();
-    $('protips-container').show();
+    $('#protips-container').show();
     $('#race-time-container').hide();
   });
 
   $('.race-time-dropdown').on('click', function(event) {
     console.log("you clicked race time");
+    $('#about-me').hide();
     $("#sitter-swap-container").hide();
     $('#within-reach-container').hide();
-    $('protips-container').hide();
+    $('#protips-container').hide();
     $('#race-time-container').show();
   });
 
