6d1ad051fdd712c60512a22e49f740ff1fcc1919
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-11-15 18:24:35 -0500

    adds angular routing file

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 // sanity check
 console.log('app.js linked');
 
-var portfolio = angular.module('portfolio', []);
+var portfolio = angular.module('portfolio', [$ngRoute]);
 
 portfolio.config(function() {
 
 });
 
 portfolio.controller('ProjectsController', ['$scope', function($scope) {
+
+  $scope.class = "inactive";
+
+  $scope.changeClass = function () {
+    if($scope.class === "active")
+      $scope.class = "inactive";
+    else
+      $scope.class = "active";
+  };
 
 }]);
