319656b76be906e4e9e6832dd350bc5acb5aeb2f
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-11-17 09:37:58 -0800

    testing views routing

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 // sanity check
 console.log('app.js linked');
 
-var portfolio = angular.module('portfolio', [$ngRoute]);
+var portfolio = angular.module('portfolio', ['ngRoute']);
 
-portfolio.config(function() {
+portfolio.config(['$routeProvider', function($routeProvider) {
 
-});
-
-portfolio.controller('ProjectsController', ['$scope', function($scope) {
-
-  $scope.class = "inactive";
-
-  $scope.changeClass = function () {
-    if($scope.class === "active")
-      $scope.class = "inactive";
-    else
-      $scope.class = "active";
-  };
-
+  $routeProvider
+    .when('/', {
+      templateurl: 'views/about-me.html'
+    })
+    .when('/contact-me', {
+      templateurl: 'views/contact-me.html'
+    })
+    .when('/sitterswap', {
+      templateurl: 'views/sitterswap.html'
+    })
+    .when('/within-reach', {
+      templateurl: 'views/within-reach.html'
+    })
+    .when('/protips', {
+      templateurl: 'views/protips.html'
+    })
+    .when('/race-time', {
+      templateurl: 'views/race-time.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
 }]);
+
+// portfolio.controller('ProjectsController', ['$scope', function($scope) {
+
+  // $scope.class = "inactive";
+  //
+  // $scope.changeClass = function () {
+  //   if($scope.class === "active")
+  //     $scope.class = "inactive";
+  //   else
+  //     $scope.class = "active";
+  // };
+
+// }]);
